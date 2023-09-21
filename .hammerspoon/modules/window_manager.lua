f388c97df9136ef27d59fce4e6fbdead01f3ac6f
cmygray <cmygray@users.noreply.github.com>
2023-09-21 09:19:24 +0900

    hammerspoon: 윈도우 매니저 듀얼모니터 대응
    Signed-off-by: cmygray <cmygray@users.noreply.github.com>

--- .hammerspoon/modules/window_manager.lua
+++ .hammerspoon/modules/window_manager.lua
@@ -7,6 +7,10 @@ function get_frame ()
       y = 0,
       w = 0,
       h = 0,
+      left_w = 0,
+      left_h = 0,
+      right_w = 0,
+      right_h = 0
     }
 
     local left_frame = hs.screen.allScreens()[1]:frame()
@@ -16,12 +20,16 @@ function get_frame ()
     frame.y = left_frame.y
     frame.w = left_frame.w + right_frame.w
     frame.h = left_frame.h
+    frame.left_w = left_frame.w
+    frame.left_h = left_frame.h
+    frame.right_w = right_frame.w
+    frame.right_h = right_frame.h
 
     return frame
   end
 end
 
-hs.hotkey.bind({"ctrl", "alt"}, "return", function()
+hs.hotkey.bind({"ctrl", "shift", "alt"}, "return", function()
   local frame = get_frame()
 
   local window = hs.window.focusedWindow()
@@ -35,7 +43,7 @@ hs.hotkey.bind({"ctrl", "alt"}, "return", function()
   window:setFrame(window_frame)
 end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "left", function()
+hs.hotkey.bind({"ctrl", "shift", "alt"}, "left", function()
   local frame = get_frame()
 
   local window = hs.window.focusedWindow()
@@ -43,153 +51,153 @@ hs.hotkey.bind({"ctrl", "alt"}, "left", function()
 
   window_frame.x = frame.x
   window_frame.y = frame.y
-  window_frame.w = frame.w / 2
-  window_frame.h = frame.h
+  window_frame.w = frame.left_w
+  window_frame.h = frame.left_h
 
   window:setFrame(window_frame)
 end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "right", function()
+hs.hotkey.bind({"ctrl", "shift", "alt"}, "right", function()
   local frame = get_frame()
 
   local window = hs.window.focusedWindow()
   local window_frame = window:frame()
 
-  window_frame.x = frame.w / 2
+  window_frame.x = frame.left_w
   window_frame.y = frame.y
-  window_frame.w = frame.w / 2
-  window_frame.h = frame.h
+  window_frame.w = frame.right_w
+  window_frame.h = frame.right_h
 
   window:setFrame(window_frame)
 end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "e", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "e", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = frame.x
-  window_frame.y = frame.y
-  window_frame.w = frame.w / 3 * 2
-  window_frame.h = frame.h
+--  window_frame.x = frame.x
+--  window_frame.y = frame.y
+--  window_frame.w = frame.w / 3 * 2
+--  window_frame.h = frame.h
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "t", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "t", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = frame.w / 3
-  window_frame.y = frame.y
-  window_frame.w = frame.w / 3 * 2
-  window_frame.h = frame.h
+--  window_frame.x = frame.w / 3
+--  window_frame.y = frame.y
+--  window_frame.w = frame.w / 3 * 2
+--  window_frame.h = frame.h
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "d", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "d", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = frame.x
-  window_frame.y = frame.y
-  window_frame.w = frame.w / 3
-  window_frame.h = frame.h
+--  window_frame.x = frame.x
+--  window_frame.y = frame.y
+--  window_frame.w = frame.w / 3
+--  window_frame.h = frame.h
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "f", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "f", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = frame.w / 3
-  window_frame.y = frame.y
-  window_frame.w = frame.w / 3
-  window_frame.h = frame.h
+--  window_frame.x = frame.w / 3
+--  window_frame.y = frame.y
+--  window_frame.w = frame.w / 3
+--  window_frame.h = frame.h
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "g", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "g", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = frame.w / 3 * 2
-  window_frame.y = frame.y
-  window_frame.w = frame.w / 3
-  window_frame.h = frame.h
+--  window_frame.x = frame.w / 3 * 2
+--  window_frame.y = frame.y
+--  window_frame.w = frame.w / 3
+--  window_frame.h = frame.h
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "u", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "u", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = frame.x
-  window_frame.y = frame.y
-  window_frame.w = frame.w / 2
-  window_frame.h = frame.h / 2
+--  window_frame.x = frame.x
+--  window_frame.y = frame.y
+--  window_frame.w = frame.w / 2
+--  window_frame.h = frame.h / 2
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "j", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "j", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = frame.x
-  window_frame.y = frame.y + frame.h / 2
-  window_frame.w = frame.w / 2
-  window_frame.h = frame.h / 2
+--  window_frame.x = frame.x
+--  window_frame.y = frame.y + frame.h / 2
+--  window_frame.w = frame.w / 2
+--  window_frame.h = frame.h / 2
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "i", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "i", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = frame.w / 2
-  window_frame.y = frame.y
-  window_frame.w = frame.w / 2
-  window_frame.h = frame.h / 2
+--  window_frame.x = frame.w / 2
+--  window_frame.y = frame.y
+--  window_frame.w = frame.w / 2
+--  window_frame.h = frame.h / 2
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "k", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "k", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = frame.w / 2
-  window_frame.y = frame.y + frame.h / 2
-  window_frame.w = frame.w / 2
-  window_frame.h = frame.h / 2
+--  window_frame.x = frame.w / 2
+--  window_frame.y = frame.y + frame.h / 2
+--  window_frame.w = frame.w / 2
+--  window_frame.h = frame.h / 2
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "up", function()
+hs.hotkey.bind({"ctrl", "shift", "alt"}, "up", function()
   local frame = get_frame()
 
   local window = hs.window.focusedWindow()
@@ -203,7 +211,7 @@ hs.hotkey.bind({"ctrl", "alt"}, "up", function()
   window:setFrame(window_frame)
 end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "down", function()
+hs.hotkey.bind({"ctrl", "shift", "alt"}, "down", function()
   local frame = get_frame()
 
   local window = hs.window.focusedWindow()
@@ -217,15 +225,15 @@ hs.hotkey.bind({"ctrl", "alt"}, "down", function()
   window:setFrame(window_frame)
 end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "c", function()
-  local frame = get_frame()
+--hs.hotkey.bind({"ctrl", "alt"}, "c", function()
+--  local frame = get_frame()
 
-  local window = hs.window.focusedWindow()
-  local window_frame = window:frame()
+--  local window = hs.window.focusedWindow()
+--  local window_frame = window:frame()
 
-  window_frame.x = (frame.w - window_frame.w) / 2
-  window_frame.y = (frame.h - window_frame.h) / 2
+--  window_frame.x = (frame.w - window_frame.w) / 2
+--  window_frame.y = (frame.h - window_frame.h) / 2
 
-  window:setFrame(window_frame)
-end)
+--  window:setFrame(window_frame)
+--end)
 
