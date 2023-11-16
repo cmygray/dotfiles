ebe5afecd27bcffc53eaf557570050e24c7b07e6
cmygray <cmygray@users.noreply.github.com>
2023-11-16 15:00:22 +0900

    bootstrap: update missing configuration sym links
    Signed-off-by: cmygray <cmygray@users.noreply.github.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,16 @@ brew bundle --file=$HOME/dotfiles/Brewfile
 brew cleanup
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-[ ! -f $HOME/Brewfile ] && ln -nfs $HOME/dotfiles/Brewfile $HOME/Brewfile
+ln -nfs $HOME/dotfiles/Brewfile $HOME/Brewfile
 
-[ ! -f $HOME/.config/starship.toml ] && ln -nfs $HOME/dotfiles/starship.toml $HOME/.config/starship.toml
+ln -nfs $HOME/dotfiles/starship.toml $HOME/.config/starship.toml
 
-[ ! -f $HOME/.gitconfig ] && ln -nfs $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-[ ! -f $HOME/.gitignore ] && ln -nfs $HOME/dotfiles/.gitignore $HOME/.gitignore
+ln -nfs $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -nfs $HOME/dotfiles/.gitignore $HOME/.gitignore
 
-rm -rf $HOME/.hammerspoon && ln -nfs $HOME/dotfiles/.hammerspoon $HOME/.hammerspoon
+ln -nfs $HOME/dotfiles/gh-dash $HOME/.config/gh-dash
+
+ln -nfs $HOME/dotfiles/.hammerspoon $HOME/.hammerspoon
+
+ln -nfs $HOME/dotfiles/.wezterm.lua $HOME/.wezterm.lua
 
